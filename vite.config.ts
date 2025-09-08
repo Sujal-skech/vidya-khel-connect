4b281a9a2f64f91c4f8c395181be72c12b811d7f
Sujal-skech <sujal8423306557@gmail.com>
2025-09-08 22:12:34 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/eduquest/", // 👈 Must match your GitHub repo name
+  host: "0",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
